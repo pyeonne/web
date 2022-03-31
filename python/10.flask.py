3bfcd9d885345d5616d19c4614db585faeb3f476
pyeonne <pyeonne@gmail.com>
2022-03-31 14:04:29 +0900

    [📚 Docs]: 데이터베이스 Python 연동

--- python/10.flask.py
+++ python/10.flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
@@ -8,7 +8,9 @@ topics = [
   {'id': 3, 'title': 'js', 'body': 'js is ...'}
 ]
 
-def template(content):
+nextId = 4
+
+def template(content, id=None):
   liTags = ''
   for topic in topics:
     liTags = liTags + f"<li><a href='/read/{topic['id']}'>{topic['title']}</a></li>"
@@ -22,6 +24,12 @@ def template(content):
     {content}
     <ul>
       <li><a href='/create/'>create</a></li>
+      <li><a href='/update/{id}'>update</a></li>
+      <li>
+        <form action='/delete/{id}/' method='POST'>
+          <input type="submit" value="delete">
+        </form>
+      </li>
     </ul>
     </body>
   </html>
@@ -40,25 +48,61 @@ def read(id):
       title = topic['title']
       body = topic['body']
       break;
-  return template(f'<h2>{title}</h2>{body}')
+  return template(f'<h2>{title}</h2>{body}', id)
 
 @app.route("/create/")
 def create():
   content = '''
-    <form action='/create/'>
-      <p><input type='text' name='title' placeholder='title'></p>
-      <p><textarea placeholder='body' name='body'></textarea></p>
-      <p><input type='submit' value='create'></p>
+    <form action="/create_process/" method='POST'>
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p><textarea placeholder="body" name="body"></textarea></p>
+      <p><input type="submit" value="create"></p>
     </form>
   '''
   return template(content)
 
-@app.route("/update/")
-def update():
-  return 'Update'
+@app.route("/create_process/", methods=['POST'])
+def ceate_process():
+  global nextId
+  title = request.form['title']
+  body = request.form['body']
+  newTopic = {"id": nextId, "title": title, "body": body}
+  topics.append(newTopic)
+  nextId += 1
+  return redirect(f'/read/{nextId-1}/')
 
-@app.route("/delete/")
-def delete():
-  return 'Delete'
+@app.route("/update/<int:id>/")
+def update(id):
+  for topic in topics:
+    if topic['id'] == id:
+      title = topic['title']
+      body = topic['body']
+      break;
+  content = f'''
+    <form action="/update_process/{id}" method='POST'>
+      <p><input type="text" name="title" placeholder="title" value={title}></p>
+      <p><textarea placeholder="body" name="body">{body}</textarea></p>
+      <p><input type="submit" value="update"></p>
+    </form>
+  '''
+  return template(content)
+
+@app.route("/update_process/<int:id>", methods=['POST'])
+def update_process(id):
+  title = request.form['title']
+  body = request.form['body']
+  for topic in topics:
+    if topic['id'] == id:
+      topic['title'] = title
+      topic['body'] = body
+  return redirect(f'/read/{id}/')
+
+@app.route("/delete/<int:id>/", methods=['POST'])
+def delete(id):
+  for topic in topics:
+    if topic['id'] == id:
+      topics.remove(topic)
+      break;
+  return redirect('/')
 
 app.run()
